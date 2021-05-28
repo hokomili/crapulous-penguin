a1bf395485beae2c07e79ee6b31a779e8f72ade4
Rand-max <57353839+Rand-max@users.noreply.github.com>
2021-05-28 19:26:41 +0800

    add

---
+++ Assets/script/cmcontroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cmcontroller : MonoBehaviour
+{
+    public GameObject target;
+    public float Xoffset, Yoffset, Zoffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + new Vector3(-Xoffset, Yoffset, Zoffset);
+        transform.LookAt(target.transform.position);
+    }
+}
---
+++ Assets/script/trailcontroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trailcontroller : MonoBehaviour
+{
+    private TrailRenderer trail;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trail = GetComponent<TrailRenderer>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            trail.startColor = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            trail.startColor = Color.cyan;
+        }
+    }
+
+}

7d1337cac41ce77b0e3ee68056700365355ef238
hokomili <28939939+hokomili@users.noreply.github.com>
2021-06-03 04:28:07 +0800

    item system

--- Assets/script/playercontroler.cs
+++ Assets/script/playercontroler.cs
@@ -16,15 +16,26 @@ public class playercontroler : MonoBehaviour {
 	public GameObject enemytrail;
 	//private int[][] triangles;
 	//private MeshRenderer[] meh;
+	public GameObject exfield;
+	public AudioClip[] audioref;
+	public GameObject sound;
+	public int currentskill=0;
+	public int skillcount=0;
 	private string axistringx;
 	private string axistringy;
+	private string function;
+	
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
 		axistringx="Horizontal"+player;
 		axistringy="Vertical"+player;
+		function="Function"+player;
 	}
 	void FixedUpdate (){
+		if(skillcount==0){
+			currentskill=0;
+		}
 		float movex = Input.GetAxis(axistringx);
 		float movez = Input.GetAxis(axistringy);
 		Vector3 movement;
@@ -56,15 +67,30 @@ public class playercontroler : MonoBehaviour {
 			//transform.eulerAngles=new Vector3(-rb.velocity.y,transform.eulerAngles.y,0f);
 		}
 		transform.Rotate(new Vector3(0f,movex*rotationalspeed,0f));
-		if (Input.GetKeyDown("space")==true&&(cooldown<=0)){
-			cooldown=1f;
-			movement = new Vector3 (rb.transform.forward.x*100, rb.transform.forward.y*100+980f,rb.transform.forward.z*100);
-		}
-		else if(Input.GetKeyDown("left shift")==true){
-			movement = new Vector3 (rb.transform.forward.x*1000, rb.transform.forward.y*1000-49f, rb.transform.forward.z*1000);
-		}
-		else{
-			movement = new Vector3 (rb.transform.forward.x*100, rb.transform.forward.y*100-49f, rb.transform.forward.z*100);
+		movement = new Vector3 (rb.transform.forward.x*100, rb.transform.forward.y*100-49f, rb.transform.forward.z*100);
+		if (Input.GetAxis(function)>0&&(cooldown<=0)){
+			skillcount--;
+			switch(currentskill){
+				case 1:
+				Playsound(1);
+				cooldown=1f;
+				movement = new Vector3 (rb.transform.forward.x*100, rb.transform.forward.y*100+1470f,rb.transform.forward.z*100);
+				break;
+				case 2:
+				Playsound(2,0.3f);
+				cooldown=1f;
+				movement = new Vector3 (rb.transform.forward.x*1000, rb.transform.forward.y*1000-49f, rb.transform.forward.z*1000);
+				break;
+				case 3:
+				Playsound(3);
+				cooldown=1f;
+				GameObject exfie = Instantiate(exfield, this.gameObject.transform.position, Quaternion.identity);
+				exfie.transform.SetParent(this.transform);
+				exfie.transform.tag=this.tag;
+				//Instantiate(sparkles, this.gameObject.transform.position, Quaternion.identity);
+				
+				break;
+			}
 		}
 		if(cooldown>0){
 			cooldown-=Time.deltaTime;
@@ -74,5 +100,32 @@ public class playercontroler : MonoBehaviour {
 	private void OnTriggerEnter(Collider other)
     {
 		if(other.transform.tag==enemytrail.transform.tag)Debug.Log("enemy");
+		switch(other.gameObject.tag){
+			case "apple":
+			currentskill=1;
+			skillcount=3;
+            Playsound(0);
+			break;
+			case "banana":
+			currentskill=2;
+			skillcount=3;
+            Playsound(0);
+			break;
+			case "bomb":
+			currentskill=3;
+			skillcount=3;
+            Playsound(0);
+			break;
+        }
+    }
+	void Playsound(int index,float offset=0f)
+    {
+		AudioSource[] sounds= sound.GetComponents<AudioSource>();
+		for(int i=0;i<sounds.Length;i++){
+			if(sounds[i].clip==audioref[index]){
+				sounds[i].time = offset;
+				sounds[i].Play();
+			}
+		}
     }
 }

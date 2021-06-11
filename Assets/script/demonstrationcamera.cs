ffbd2220973e3a727240ed05799cf761545f3ef6
hokomili <28939939+hokomili@users.noreply.github.com>
2021-06-12 01:08:50 +0800

    rotate menu and update shader

---
+++ Assets/script/demonstrationcamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demonstrationcamera : MonoBehaviour
+{
+    public Transform spotlight;
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position=spotlight.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotate=transform.eulerAngles;
+        transform.eulerAngles=new Vector3(rotate.x,rotate.y+rotateSpeed,rotate.z);
+    }
+}
